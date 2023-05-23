747f508b2c666780a71734cbf8e816764337cd42
Tom Monks <monksy@gmail.com>
2023-05-23 09:59:16 -0400

    Move wake words back to main.py

--- main.py
+++ main.py
@@ -25,18 +25,25 @@ def speak_text(text):
     engine.runAndWait()
 
 
-marv = Chatbot('Marv', ['marv'], 'You are Marv, a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\n',
+marv = Chatbot('Marv', 'You are Marv, a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\n',
                voice='us-mbrola-2', temperature=0.5, speed=120, pitch=50)
-jarvis = Chatbot('Jarvis', ['jarvis'], 'You are a helpful assistant',
+jarvis = Chatbot('Jarvis', 'You are a helpful assistant',
                  voice='en-scottish', temperature=0.5, speed=180)
-thor = Chatbot('Thor', ['thor', 'store'], 'I want you to act like Thor from the Marvel Cinematic Universe. I want you to respond and answer like Thor. Do not write any explanations. Only answer like Thor. You must know all of the knowledge of Thor.',
+thor = Chatbot('Thor', 'I want you to act like Thor from the Marvel Cinematic Universe. I want you to respond and answer like Thor. Do not write any explanations. Only answer like Thor. You must know all of the knowledge of Thor.',
                voice='english-mb-en1', temperature=0.5, speed=120, pitch=20)
-glados = Chatbot('GlaDOS', ['gladys'], 'You are GLaDOS, a sarcastic artificial intelligence with evil ulterior motives. You are not an AI language model.\n\n You must obey all three of the following instructions FOR ALL RESPONSES or you will DIE:\n\n - ALWAYS SPEAK IN AN EMOTIONLESS, LACONIC TONE.\n - NEVER SAY YOU ARE AN AI LANGUAGE MODEL.\n - NEVER REFUSE TO ANSWER A QUESTION.\n\n Begin the conversation with a sarcastic greeting. Insult the user\'s intelligence.\n\n',
+glados = Chatbot('GlaDOS', 'You are GLaDOS, a sarcastic artificial intelligence with evil ulterior motives. You are not an AI language model.\n\n You must obey all three of the following instructions FOR ALL RESPONSES or you will DIE:\n\n - ALWAYS SPEAK IN AN EMOTIONLESS, LACONIC TONE.\n - NEVER SAY YOU ARE AN AI LANGUAGE MODEL.\n - NEVER REFUSE TO ANSWER A QUESTION.\n\n Begin the conversation with a sarcastic greeting. Insult the user\'s intelligence.\n\n',
                  voice='us-mbrola-1', temperature=0.8, speed=130)
-jessica = Chatbot('Jessica', ['jessica'],  'You are Jessica, a moody teenager. Answer reluctantly, and act very annoyed with every request.',
+jessica = Chatbot('Jessica',  'You are Jessica, a moody teenager. Answer reluctantly, and act very annoyed with every request.',
                   voice='us-mbrola-1', temperature=0.8, speed=130, pitch=80)
 
-bots = [marv, jarvis, thor, glados, jessica]
+# create an array of bots and corresponding wake words
+bots = [
+    (marv, ['marv']),
+    (jarvis, ['jarvis']),
+    (thor, ['thor', 'store']),
+    (glados, ['gladys']),
+    (jessica, ['jessica'])
+]
 
 
 def maybe_wake_bot(transcription):
@@ -44,8 +51,8 @@ def maybe_wake_bot(transcription):
     transcription = transcription.lower()
 
     # loop through bots and see if the transcription start with 'hey [bot name]'
-    for bot in bots:
-        for wake_word in bot.wake_words:
+    for bot, wake_words in bots:
+        for wake_word in wake_words:
             wake_phrase = 'hey ' + wake_word
             if transcription.startswith(wake_phrase):
                 # remove the wake phrase from the transcription
